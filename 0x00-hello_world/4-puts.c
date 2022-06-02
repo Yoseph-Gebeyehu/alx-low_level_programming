1daf5cb3467e7baa198232d45660c4ee8d741be0
Yoseph Gebeyehu <yosephgebeyehu73@gmail.com>
2022-06-02 14:57:02 -0700

    The Hello,puts and the Name tasks are done

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+
+ * main - Using printf.
+
+(*
+
+ * Return: 0 on success
+
+ */
+
+int main(void)
+
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+
+
+
+
+	return (0);
+
+}
