e6da5e96f003cdce3843ac175a85eac946fb94e8
Yoseph Gebeyehu <yosephgebeyehu73@gmail.com>
2022-06-05 09:15:27 -0700

    The 5th task is modified

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,25 +1,11 @@
 #include <stdio.h>
 
 /**
-
- * main - Using printf.
-
-(*
-
- * Return: 0 on success
-
+ *main - Entry point
+ *Return: Always 0 (Success)
  */
-
 int main(void)
-
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
-
-
-
-
-
 	return (0);
-
 }
