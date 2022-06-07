eabfb9754eb425daf29d6e8d752f2f379628642d
Yoseph Gebeyehu <yosephgebeyehu73@gmail.com>
2022-06-07 12:33:47 -0700

    The 7th task is edited

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,25 +1,15 @@
 #include "main.h"
 /**
- * print_sign - cheks lowercase letters!.
- *@c: input to be tested.
+ * _abs - cheks lowercase letters!.
+ *@a: input to be tested.
  * Return: 0 on success
  */
-int print_sign(int c)
+int _abs(int a)
 {
-	if (c > 0)
+	if (a < 0)
 	{
+		return (-1 * a);
 		_putchar('+');
-		return (1);
 	}
-	else if (c == 0)
-	{
-		_putchar('0');
-		return (0);
-	}
-	else if (c < 0)
-	{
-		_putchar('-');
-		return (-1);
-	}
-	return (0);
+	return (a);
 }
