2aa0ee7f814e9eb5cac247a52ecb25e735443ebb
Yoseph Gebeyehu <yosephgebeyehu73@gmail.com>
2022-06-20 23:36:33 -0700

    One of the advanced task is done

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * set_string - adds the diagonal elements of mat.
+ * @s: pointer to hold char;
+ * @to: a char that contains the chracter.
+ * Return: nothing.
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
+
